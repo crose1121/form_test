6b6921fde8c2cd802868567a7d4c1052c89e91d7
crose1121 <coreycoding1121@outlook.com>
2021-12-06 07:30:31 -0500

    initial commit

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/form')
+def form():
+    return render_template('forms.html')
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    print("Got POST info")
+    print(request.form)
+    return redirect('/form')
+
+if __name__ == '__main__':
+    app.run(debug=True)
